53bf12fbb5b579da893e08bf8863c165c122d100
PC23\MehmetEROZKAN <mehmeterozkan@gmail.com>
2017-05-09 09:59:29 +0300

    1.update

---
+++ Bulbizi/Bulbizi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bulbizi.Startup))]
+namespace Bulbizi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
